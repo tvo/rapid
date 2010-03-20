be7ecef9aaef2ad5cc20fb3bfda5e383390ff738
Rene Milk <koshi@springlobby.info>
2010-03-20 16:27:49 +0100

    GUI: show both installed and available widgets

--- gui.py
+++ gui.py
@@ -4,10 +4,10 @@
 from PyQt4 import QtCore, QtGui
 import models
 
-class RapidListWidget(QtGui.QWidget):
+class RapidListWidgetBase(QtGui.QWidget):
 	"""mostly based on the bascisortfiltermodel example from PyQt distribution"""
 	def __init__(self,parent):
-		super(RapidListWidget, self).__init__(parent)
+		super(RapidListWidgetBase, self).__init__(parent)
 
 		self.proxyModel = QtGui.QSortFilterProxyModel()
 		self.proxyModel.setDynamicSortFilter(True)
@@ -17,17 +17,42 @@ class RapidListWidget(QtGui.QWidget):
 		self.proxyView.setAlternatingRowColors(True)
 		self.proxyView.setModel(self.proxyModel)
 		self.proxyView.setSortingEnabled(True)
-		self.proxyModel.setSourceModel(models.RapidModel(self))
 		self.proxyView.sortByColumn(0, QtCore.Qt.AscendingOrder)
-
+		
 		mainLayout = QtGui.QVBoxLayout()
 		mainLayout.addWidget(self.proxyView)
 		self.setLayout(mainLayout)
 
+class InstalledRapidListWidget(RapidListWidgetBase):
+	def __init__(self,parent):
+		super(InstalledRapidListWidget, self).__init__(parent)
+		self.sourceModel = models.InstalledRapidModel(self)
+		self.proxyModel.setSourceModel( self.sourceModel )
+		self.sourceModel.reload()
+
+class AvailableRapidListWidget(RapidListWidgetBase):
+	def __init__(self,parent):
+		super(AvailableRapidListWidget, self).__init__(parent)
+		self.sourceModel = models.AvailableRapidModel(self)
+		self.proxyModel.setSourceModel( self.sourceModel )
+		self.sourceModel.reload()
+
+class MainRapidWidget(QtGui.QWidget):
+	def __init__(self,parent):
+		super(MainRapidWidget, self).__init__(parent)
+		self.availableWidget = AvailableRapidListWidget(self)
+		self.installedWidget = InstalledRapidListWidget(self)
+		mainLayout = QtGui.QHBoxLayout(self)
+		mainLayout.addWidget( self.availableWidget )
+		mainLayout.addWidget( self.installedWidget )
+		self.setLayout( mainLayout )
+		self.setMinimumSize(1034,768)
+
 class RapidGUI(QtGui.QMainWindow):
 	def __init__(self):
 		QtGui.QMainWindow.__init__(self)
-		self.setCentralWidget(RapidListWidget(self))		
+		self.mainWidget = MainRapidWidget(self)
+		self.setCentralWidget(self.mainWidget)
 
 if __name__ == '__main__':
 	app = QtGui.QApplication(['RapidGUI'])
--- models.py
+++ models.py
@@ -4,15 +4,30 @@
 from PyQt4 import QtCore, QtGui
 import rapid,main
 
-class RapidModel(QtGui.QStandardItemModel):
+class BaseRapidModel(QtGui.QStandardItemModel):
 	def __init__(self,parent):
-		super(RapidModel,self).__init__(0, 2, parent)
+		super(BaseRapidModel,self).__init__(0, 2, parent)
 		self.setHeaderData(0, QtCore.Qt.Horizontal, "Name")
 		self.setHeaderData(1, QtCore.Qt.Horizontal, "Tag")
+
+	def reloadData(self, dataFunction):
 		i = 0
-		for p in filter(lambda p: not p.installed(), main.rapid.get_packages() ):
+		for p in filter( dataFunction, main.rapid.get_packages() ):
 			self.insertRow(i)
 			self.setData(self.index(i, 0), p.name)
 			self.setData(self.index(i, 1), p.tag)
 			i += 1
 
+class AvailableRapidModel(BaseRapidModel):
+	def __init__(self,parent):
+		super(AvailableRapidModel, self).__init__(parent)
+
+	def reload(self):
+		self.reloadData( lambda p: not p.installed() )
+
+class InstalledRapidModel(BaseRapidModel):
+	def __init__(self,parent):
+		super(InstalledRapidModel, self).__init__(parent)
+
+	def reload(self):
+		self.reloadData( lambda p: p.installed() )
