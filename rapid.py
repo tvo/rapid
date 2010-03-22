94d7edfd38b1b4d4ef15d0a52f88467b90318dbd
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-03-22 15:44:08 +0100

    cache packages locally and other improvements
    such as
    * normalise versions.gz data
    * list comprehensions instead of map/filter pairs

--- rapid.py
+++ rapid.py
@@ -12,6 +12,7 @@ import gzip, os, struct
 
 from downloader import Downloader, atomic_write
 
+################################################################################
 
 # content_dir : Storage for temporary files (repos.gz, versions.gz)
 # spring_dir  : Spring data directory
@@ -28,6 +29,7 @@ if os.name == 'posix':
 else:
 	raise NotImplementedError('Unknown OS: %s' % os.name)
 
+################################################################################
 
 def mkdir(path):
 	""" Create directory if it does not exist yet. """
@@ -38,6 +40,12 @@ def mkdir_p(path):
 	""" Create directories if they do not exist yet. """
 	if not os.path.exists(path):
 		os.makedirs(path)
+
+def psv(s):
+	""" Split pipe separated value string into list of non-empty components."""
+	return [x for x in s.split('|') if x]
+
+################################################################################
 
 class RapidException(Exception):
 	""" Base class for other exceptions defined in this module."""
@@ -61,15 +69,18 @@ class StreamerFormatException(RapidException):
 	def __str__(self):
 		return self.field
 
+################################################################################
 
 class Rapid:
 	""" Repository container."""
-	master_url = 'http://repos.caspring.org/repos.gz'
-	cache_dir = content_dir
 	__repositories = None
 	__packages = None
 
 	def __init__(self):
+		self.master_url = 'http://repos.caspring.org/repos.gz'
+		self.cache_dir = content_dir
+		self.packages_gz = os.path.join(self.cache_dir, 'packages.gz')
+
 		mkdir(content_dir)
 		mkdir(spring_dir)
 		mkdir(package_dir)
@@ -95,26 +106,65 @@ class Rapid:
 			return self.__repositories
 
 		with closing(gzip.open(self.update())) as f:
-			unique = set(map(lambda x: x.split(',')[1], f))
-			self.__repositories = map(lambda x: Repository(self, x), unique)
+			unique = set([x.split(',')[1] for x in f])
+			self.__repositories = [Repository(self, x) for x in unique]
 
 		return self.__repositories
+
+	def read_packages_gz(self):
+		""" Reads global packages.gz into a dictionary of Packages.
+
+			Contrary to versions.gz, packages.gz:
+			- is normalised (i.e. every package occurs only once),
+			- does not support '|' characters in tags (tags are '|' separated)
+		"""
+		def read_line(line):
+			row = line[:-1].split(',')
+			return (row[3], Package(tags         = psv(row[0]), hex  = row[1],
+			                        dependencies = psv(row[2]), name = row[3]))
+
+		if os.path.exists(self.packages_gz):
+			with closing(gzip.open(self.packages_gz)) as f:
+				return dict(map(read_line, f))
+		return {}
+
+	def write_packages_gz(self):
+		# Write to temporary file
+		tempfile = self.packages_gz + '.tmp'
+		with closing(gzip.open(tempfile, 'wb')) as f:
+			for p in self.__packages.itervalues():
+				# tags, hex, dependencies, name
+				f.write(','.join(['|'.join(p.tags), p.hex, '|'.join(p.dependencies), p.name]) + '\n')
+
+		# Commit by moving temporary file over packages.gz
+		if os.path.exists(tempfile):
+			if os.path.exists(self.packages_gz):
+				os.remove(self.packages_gz)
+			os.rename(tempfile, self.packages_gz)
 
 	def get_packages(self):
 		""" Get combined list of packages published by all repositories."""
 		if self.__packages:
 			return self.__packages
 
-		self.__packages = reduce(lambda x, y: x + y.get_packages(), self.get_repositories(), [])
+		# First call to get_repositories() clobbers __packages and __repositories!
+		self.get_repositories()
+		self.__packages = self.read_packages_gz()
+
+		# FIXME: this is broken if a package is in repo1 with tag1 and in repo2 with tag2
+		for r in self.get_repositories():
+			self.__packages.update(r.get_packages())
+
+		self.write_packages_gz()
 
 		# Resolve dependencies.
 		# Dependencies missing in all repositories are silently discarded.
-		by_name = dict(map(lambda x: (x.name, x), self.__packages))
-		for p in self.__packages:
-			p.dependencies = map(lambda x: by_name[x], filter(lambda x: by_name.has_key(x), p.dependencies))
+		for p in self.__packages.itervalues():
+			p.dependencies = [self.__packages[name] for name in p.dependencies if name in self.__packages]
 
 		return self.__packages
 
+################################################################################
 
 class Repository:
 	""" A rapid package repository."""
@@ -125,17 +175,34 @@ class Repository:
 		self.url = url
 		self.cache_dir = os.path.join(self.rapid.cache_dir, urlparse(url).netloc)
 		self.package_cache_dir = os.path.join(self.cache_dir, 'packages')
+		self.versions_gz = os.path.join(self.cache_dir, 'versions.gz')
 
-		# Create cache directories
 		mkdir(self.cache_dir)
 		mkdir(self.package_cache_dir)
 
 	def update(self):
 		""" Force an update of the list of packages of this repository."""
-		path = os.path.join(self.cache_dir, 'versions.gz')
-		self.rapid.downloader.conditional_get_request(self.url + '/versions.gz', path)
+		self.rapid.downloader.conditional_get_request(self.url + '/versions.gz', self.versions_gz)
 		self.__packages = None
-		return path
+
+	def read_versions_gz(self):
+		""" Reads versions.gz-formatted file into a dictionary of Packages."""
+		packages = {}
+
+		def read_line(line):
+			row = line[:-1].split(',')   # tag,hex,dependencies,name
+			tag, hex, deps, name = row[0], row[1], psv(row[2]), row[3]
+			if not name in packages:
+				packages[name] = Package(hex, name, deps, repository = self)
+			assert (packages[name].hex == hex)
+			assert (packages[name].dependencies == deps)
+			assert (packages[name].name == name)
+			packages[name].tags.add(tag)
+
+		with closing(gzip.open(self.versions_gz)) as f:
+			map(read_line, f)
+
+		return packages
 
 	def get_packages(self):
 		""" Download and return the list of packages offered. For these
@@ -144,27 +211,23 @@ class Repository:
 		if self.__packages:
 			return self.__packages
 
-		def read_line(line):
-			row = line[:-1].split(',')   # tag,hex,dependencies,name
-			deps = filter(lambda x: len(x)>0, row[2].split('|'))
-			return Package(self, row[0], row[1], deps, row[3])
-
-		with closing(gzip.open(self.update())) as f:
-			self.__packages = map(read_line, f)
-
+		self.update()
+		self.__packages = self.read_versions_gz()
 		return self.__packages
 
+################################################################################
 
 class Package:
 	__files = None
 
-	def __init__(self, repository, tag, hex, dependencies, name):
-		self.repository = repository
-		self.tag = tag
+	def __init__(self, hex, name, dependencies, tags = None, repository = None):
 		self.hex = hex
-		self.dependencies = dependencies
 		self.name = name
-		self.cache_file = os.path.join(repository.cache_dir, 'packages', self.hex + '.sdp')
+		self.dependencies = dependencies
+		self.tags = set(tags or [])
+		self.repository = repository
+		if repository:
+			self.cache_file = os.path.join(repository.package_cache_dir, self.hex + '.sdp')
 
 	def get_installed_path(self):
 		""" Return the path at which the package would be visible to Spring."""
@@ -295,6 +358,7 @@ class Package:
 		""" Return true if the package is installed, false otherwise."""
 		return os.path.exists(self.get_installed_path())
 
+################################################################################
 
 class File:
 	def __init__(self, package, name, md5, crc32, size):
@@ -312,24 +376,3 @@ class File:
 	def available(self):
 		""" Return true iff the file is available locally."""
 		return os.path.exists(self.get_pool_path())
-
-
-if __name__ == '__main__':
-	from progressbar import ProgressBar
-	try:
-		r = Rapid()
-		"""for r in r.get_repositories():
-			print(r)
-			for p in r.get_packages():
-				print(p.tag, p.hex, p.name)
-				for d in p.dependencies:
-					print (p.tag, p.name, type(d), str(d))"""
-		"""for p in r.get_packages():
-			print(p.tag, p.hex, p.name)"""
-		for p in r.get_packages():
-			if 'Spring: 1944' in p.name:
-				print (p.name, len(p.get_files()), len(p.get_missing_files()))
-				p.download_files(p.get_missing_files(), ProgressBar())
-	except RapidException as e:
-		print str(e)
-		raise
