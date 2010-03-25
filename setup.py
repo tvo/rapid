72903b34107c6914a355a4f91c14b9fde5020b05
Rene Milk <koshi@springlobby.info>
2010-03-25 21:54:19 +0100

    fix module naming

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(
     name='rapid-spring',
-    version='0.1.0',
+    version='0.1.1',
     author='Tobi Vollebregt',
     author_email='',
-    packages=['rapid-spring'],
+    packages=['rapid'],
     scripts=['bin/rapid','bin/rapid-gui'],
     url='http://pypi.python.org/pypi/rapid-spring/',
     license='LICENSE.txt',
