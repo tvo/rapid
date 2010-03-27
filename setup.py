508c85b979ec9412315c6b6fe4f455e054799527
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-03-27 01:10:32 +0100

    bump to version 0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='rapid-spring',
-    version='0.1.2',
+    version='0.1.3',
     author='Tobi Vollebregt',
     author_email='tobivollebregt@gmail.com',
     packages=['rapid'],
