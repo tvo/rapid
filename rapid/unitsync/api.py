1a3039c96c7ee742433506ce109c82497b063c62
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-08-11 14:58:21 +0200

    unitsync: more extensive search for unitsync, based a bit on SpringLobby

--- rapid/unitsync/api.py
+++ rapid/unitsync/api.py
@@ -11,36 +11,67 @@ from unitsync import Unitsync
 class UnitsyncError(Exception):
 	pass
 
-def generate_linux_paths():
-	"""Yield candidate unitsync locations for Linux (based?) systems."""
-	yield '/usr/lib/libunitsync.so'
-	yield '/usr/local/lib/libunitsync.so'
-	yield '/opt/spring/lib/libunitsync.so'
+def generate_linux_search_paths():
+	"""Yield candidate unitsync paths for Linux (based?) systems."""
+	ldpath = os.getenv('LDPATH')
+	if ldpath:
+		for path in ldpath.split(':'):
+			yield path
 
-def generate_windows_paths():
-	"""Yield candidate unitsync locations for Windows systems."""
-	yield r'c:\Program Files\Spring\unitsync.dll'
-	#FIXME: add more common locations?
+	ld_library_path = os.getenv('LD_LIBRARY_PATH')
+	if ld_library_path:
+		for path in ld_library_path.split(':'):
+			yield ld_library_path
+
+	for prefix in ['/usr/local', '/usr/local/games', '/usr', '/usr/games']:
+		yield os.path.join(prefix, 'lib/spring')
+		yield os.path.join(prefix, 'lib64')
+		yield os.path.join(prefix, 'lib')
+
+def generate_windows_search_paths():
+	"""Yield candidate unitsync paths for Windows systems."""
+	program_files = os.getenv('ProgramFiles')
+	if program_files:
+		yield os.path.join(program_files, 'Spring')
+		yield os.path.join(program_files, 'Games', 'Spring')
+
+	yield os.getcwd()
+
+	if sys.argv[0]:
+		yield os.path.abspath(sys.argv[0])
 
 def generate_paths():
-	"""Yield candidate unitsync locations for the current operating system."""
+	"""Yield candidate unitsync paths for the current operating system."""
 	if os.name == 'posix':
-		return generate_linux_paths()
+		return generate_linux_search_paths()
 	elif os.name == 'nt':
-		return generate_windows_paths()
+		return generate_windows_search_paths()
+	else:
+		raise NotImplemented()
+
+def generate_locations():
+	"""Yields fully specified unitsync locations."""
+	if os.name == 'posix':
+		ext = '.so'
+	elif os.name == 'nt':
+		ext = '.dll'
+	else:
+		raise NotImplemented()
+
+	for path in generate_paths():
+		yield os.path.join(path, 'unitsync') + ext
+		yield os.path.join(path, 'libunitsync') + ext
 
 def locate_unitsync():
 	"""Locate and instantiate unitsync, otherwise raise UnitsyncError.
 	The unitsync library must export at least Init, UnInit and GetWritableDataDirectory."""
-	for path in generate_paths():
-		try:
-			unitsync = Unitsync(path)
-		except OSError:
-			continue
-		if (unitsync.has('Init') and
-			unitsync.has('UnInit') and
-			unitsync.has('GetWritableDataDirectory')):
-			return unitsync
+	for location in generate_locations():
+		if os.path.isfile(location):
+			unitsync = Unitsync(location)
+			if (unitsync.has('Init') and
+				unitsync.has('UnInit') and
+				unitsync.has('GetWritableDataDirectory')):
+				return unitsync
 	raise UnitsyncError('suitable unitsync library not found')
 
 def get_writable_data_directory():
@@ -55,6 +86,8 @@ def get_writable_data_directory():
 
 if __name__ == '__main__':
 	# Quick test.
+	for location in generate_locations():
+		print location
 	unitsync = locate_unitsync()
 	# This is not available in older versions of unitsync.
 	print 'Has GetMapDescription?', unitsync.has('GetMapDescription')
