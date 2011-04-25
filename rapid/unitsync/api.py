70644edce33ae12e78836b0f57e818f9b2a1acbb
abma <spam@abma.de>
2011-04-26 00:47:25 +0200

    fixed invalid path generation

--- rapid/unitsync/api.py
+++ rapid/unitsync/api.py
@@ -11,73 +11,55 @@ from unitsync import Unitsync
 class UnitsyncError(Exception):
 	pass
 
-def generate_linux_search_paths():
-	"""Yield candidate unitsync paths for Linux (based?) systems."""
-	ldpath = os.getenv('LDPATH')
-	if ldpath:
-		for path in ldpath.split(':'):
-			yield path
-
-	ld_library_path = os.getenv('LD_LIBRARY_PATH')
-	if ld_library_path:
-		for path in ld_library_path.split(':'):
-			yield path
-
-	for prefix in ['/usr/local', '/usr/local/games', '/usr', '/usr/games']:
-		yield os.path.join(prefix, 'lib/spring')
-		yield os.path.join(prefix, 'lib64')
-		yield os.path.join(prefix, 'lib')
-
-def generate_windows_search_paths():
-	"""Yield candidate unitsync paths for Windows systems."""
-	yield os.getcwd()
-	if sys.argv[0]:
-		# check in folder in which rapid.exe lives and its parent
-		path = os.path.dirname(os.path.abspath(sys.argv[0]))
-		yield path
-		yield os.path.normpath(os.path.join(path, '..'))
-	try:
-		import _winreg
-		key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r'Software\Spring')
-		value, type = _winreg.QueryValueEx(key, r'SpringEngineHelper')
-		_winreg.CloseKey(key)
-		if type == _winreg.REG_SZ:
-			yield value
-		else:
-			print 'key of unknown type'
-	except WindowsError as e:
-		print "Registry key not found: " + str(e)
-	program_files = os.getenv('ProgramFiles')
-	if program_files:
-		yield os.path.join(program_files, 'Spring')
-		yield os.path.join(program_files, 'Games', 'Spring')
-
-def generate_paths():
-	"""Yield candidate unitsync paths for the current operating system."""
-	if os.name == 'posix':
-		return generate_linux_search_paths()
-	elif os.name == 'nt':
-		return generate_windows_search_paths()
-	else:
-		raise NotImplemented()
-
 def generate_locations():
-	"""Yields fully specified unitsync locations."""
+	"""Yield possible unitsync paths for the current operating system."""
 	if os.name == 'posix':
-		ext = '.so'
-	elif os.name == 'nt':
-		ext = '.dll'
+		filenames=["libunitsync.so", "unitsync.so"]
+		for filename in filenames:
+			ldpath = os.getenv('LDPATH')
+			if ldpath:
+				for path in ldpath.split(':'):
+					yield os.path.join(path, filename)
+		for filename in filenames:
+			ld_library_path = os.getenv('LD_LIBRARY_PATH')
+			if ld_library_path:
+				for path in ld_library_path.split(':'):
+					yield os.path.join(path, filename)
+		for prefix in ['/usr/local', '/usr/local/games', '/usr', '/usr/games']:
+			for filename in filenames:
+				yield os.path.join(prefix, 'lib/spring', filename)
+			for filename in filenames:
+				yield os.path.join(prefix, 'lib64', filename)
+			for filename in filenames:
+				yield os.path.join(prefix, 'lib', filename)
+	elif os.name == 'nt': #search for unitsync.dll on win32
+		filenames=["unitsync.dll"]
+		for filename in filenames:
+			yield os.getcwd()
+			if sys.argv[0]:
+				# check in folder in which rapid.exe lives and its parent
+				path = os.path.dirname(os.path.abspath(sys.argv[0]))
+				yield os.path.join(path, filename)
+				yield os.path.normpath(os.path.join(path, '..'))
+		try:
+			import _winreg
+			key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r'Software\Spring')
+			value, type = _winreg.QueryValueEx(key, r'SpringEngineHelper')
+			_winreg.CloseKey(key)
+			if type == _winreg.REG_SZ:
+				yield value
+			else:
+				print 'key of unknown type'
+		except WindowsError as e:
+			print "Registry key not found: " + str(e)
+		program_files = os.getenv('ProgramFiles')
+		if program_files:
+			for filename in filenames:
+				yield os.path.join(program_files, 'Spring', filename)
+			for filename in filenames:
+				yield os.path.join(program_files, 'Games', 'Spring', filename)
 	else:
 		raise NotImplemented()
-
-	for path in generate_paths():
-		# FIXME: this could generate something like this C:\Spring\unitsync.dll\unitsync.dll
-		# when registry key exists + unitsync.dll doesn't exist
-		if os.path.isfile(path):
-			yield path
-		else:
-			yield os.path.join(path, 'unitsync') + ext
-			yield os.path.join(path, 'libunitsync') + ext
 
 def locate_unitsync():
 	"""Locate and instantiate unitsync, otherwise raise UnitsyncError.
